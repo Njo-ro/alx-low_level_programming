8dcb840255062baba17e9423c83d58b9cb229bda
Njo-ro <njorogeremy@gmail.com>
2022-10-19 02:32:43 -0700

    added description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point to program
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] =  """Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
