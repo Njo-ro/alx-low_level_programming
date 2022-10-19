c199de211567e9e329abe0bd32b52f09bad5f06b
Njo-ro <njorogeremy@gmail.com>
2022-10-19 03:35:22 -0700

    re-ordering types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
+	char charType;
 	int integerType;
-	float floatType;
 	long int longintegerType;
 	long long int longlongintegerType;
-	char charType;
+	float floatType;
 
+	printf("Size of char: %ld byte\n", sizeof(charType));
 	printf("Size of int: %ld bytes\n", sizeof(integerType));
 	printf("Size of long int: %ld bytes\n", sizeof(longintegerType));
 	printf("Size of long long int: %ld bytes\n", sizeof(longlongintegerType));
 	printf("Size of float: %ld bytes\n", sizeof(floatType));
-	printf("Size of char: %ld byte\n", sizeof(charType));
 
 	return (0);
 }
