4281d99075cc00a61ce3029ead95133b3bee6d1f
Njo-ro <njorogeremy@gmail.com>
2022-10-19 03:28:39 -0700

    spacing checks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,14 @@ int main(void)
 {
 	int integerType;
 	float floatType;
-	long int longintegerType;
-	long long int longlongintegerType;
+	long int long integerType;
+	long long int long long integerType;
 	char charType;
 
 	printf("Size of int: %ld bytes\n", sizeof(integerType));
-	printf("Size of long int: %ld bytes\n", sizeof(longintegerType));
-	printf("Size of long long int: %ld bytes\n", sizeof(longlongintegerType));
+	printf("Size of long int: %ld bytes\n", sizeof(long integerType));
+	printf("Size of long long int: %ld bytes\n", sizeof(long long integerType));
 	printf("Size of float: %ld bytes\n", sizeof(floatType));
-	printf("Size of double: %ld bytes\n", sizeof(doubleType));
 	printf("Size of char: %ld byte\n", sizeof(charType));
 
 	return (0);
