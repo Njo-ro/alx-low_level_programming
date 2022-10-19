d75c053e98003cbb7404f9450ef6780a75ad1e5a
Njo-ro <njorogeremy@gmail.com>
2022-10-19 03:37:25 -0700

    added (s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of int: %ld bytes\n", sizeof(integerType));
 	printf("Size of long int: %ld bytes\n", sizeof(longintegerType));
 	printf("Size of long long int: %ld bytes\n", sizeof(longlongintegerType));
-	printf("Size of float: %ld bytes\n", sizeof(floatType));
+	printf("Size of float: %ld byte (s)\n", sizeof(floatType));
 
 	return (0);
 }
