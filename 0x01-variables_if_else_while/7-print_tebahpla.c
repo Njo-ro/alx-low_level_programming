d63fead70fff4c637c259dd2f3c1ed59bad32086
Njo-ro <njorogeremy@gmail.com>
2022-10-22 13:06:36 +0000

    lowercase reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prog begins
+* Return: 0
+*/
+
+int main(void) /*beginning of prog */
+{
+        char ch = 'z';
+
+        for (ch = 'z'; ch >= 'a'; ch--)
+        putchar(ch);
+        putchar('\n');
+
+        return (0);
+}
