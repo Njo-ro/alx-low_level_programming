07fab041f9bd86f8db71ce16978d69e77c5a5a0d
Njo-ro <njorogeremy@gmail.com>
2022-10-22 11:56:40 +0000

    bett fix3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-/** 
+/**
 * main - prog begins to write alphabets
 * return: 0
 */
 
+
 int main(void)
 {
 	char ch = 'a';
+	
 	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
