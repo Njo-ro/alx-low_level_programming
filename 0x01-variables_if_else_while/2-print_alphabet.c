76a1356a2736da042380878733c06885c3f26a79
Njo-ro <njorogeremy@gmail.com>
2022-10-22 11:24:56 +0000

    putchar print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*main - prog begins
+* return:0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
